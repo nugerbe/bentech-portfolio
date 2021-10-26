3c515ed43bb954654b6e1571da25734d689bf088
Benny Nuger <nugerbe@gmail.com>
2021-10-26 17:02:20 -0400

    Adding components, enabling routes, adding imgs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LightboxModule } from 'ngx-lightbox';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    LightboxModule
+    LightboxModule,
+    RouterModule.forRoot([
+      { path: 'Home', component: HomeComponent },
+      { path: 'About', component: AboutComponent },
+      { path : '**', component: HomeComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
